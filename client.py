fff5ce7c961a14fe09cec5c34e29d94952af5d93
None <None>
2023-06-14 18:51:20 -0700

    feat: parcialemente completo

--- client.py
+++ client.py
@@ -12,8 +12,6 @@ MSS = 500
 
 ip_pkt = IP(src=ip_src, dst=ip_dst)
 
-seqs_sent = []
-
 # Função para enviar o pacote de conexão
 def begin_connection():
     """Begin connection using the three-way handshake algorithm."""
@@ -64,51 +62,57 @@ def send_pkt(pkt, file, curr_file_position):
     send(pkt / data, verbose=False)
 
 # Função para enviar o pacote de dados e receber a confirmação
-def sr1_pkt(pkt, file, curr_file_position, timeout):
-    file.seek(curr_file_position)
-    data = file.read(MSS)
+def sr_pkt(pkts, timeout):
+    respondidos, nao_respondidos = sr(pkts, timeout=timeout, verbose=False)
+    return respondidos
 
-    while True:
-        respondidos, nao_respondidos = sr(pkt / data, timeout=timeout, verbose=False)
-        if len(respondidos) != 0:
-            print("Tamanho da lista de respondidos:", respondidos)
-            print(nao_respondidos)
-            return respondidos[0][1].ack
-
-def CriaPacote(seq, ack):
-    return ip_pkt / TCP(dport=dst_port, flags='PA', seq=seq, ack=ack)
+def CriaPacote(seq, ack, data=b''):
+    return ip_pkt / TCP(dport=dst_port, flags='PA', seq=seq, ack=ack) / data
 
 def send_data(file, file_size, pkt, timeout):
     pkts_to_send = 3
     curr_file_position = 0
     curr_file_position_confirmed = 0
 
+    print(curr_file_position_confirmed, curr_file_position, file_size)
+
     # Enquanto a posição atual do arquivo for menor que o tamanho do arquivo
     while curr_file_position_confirmed < file_size:
         listaDePacotes = []
         # For para enviar os pacotes considerando o numero de pacotes que podem ser enviados
-        for _ in range(pkts_to_send - 1):
-            # Adiciona a sequencia do pacote na lista de sequencias enviadas
-            seqs_sent.append(pkt.seq)
-            # Envia o pacote
-            send_pkt(pkt, file, curr_file_position)
-            # Atualiza o numero de sequencia do pacote
-            if(curr_file_position + MSS > file_size):
-                pkt.seq = pkt.seq + (file_size - curr_file_position)
-            else:
-                pkt.seq = pkt.seq + MSS
-            # Atualiza a posição atual do arquivo não confirmada
-            curr_file_position = curr_file_position + MSS
-        
-        ack = sr1_pkt(pkt, file, curr_file_position, timeout)
+        for _ in range(pkts_to_send):
+            # Faz o seek no arquivo
+            file.seek(curr_file_position)
+            # Cria o pacote
+            var = file.read(MSS)
+            if var == b'':
+                break
+            novopkt = CriaPacote(pkt.seq, pkt.ack, var)
+            # Adiciona o pacote na lista de pacotes
+            listaDePacotes.append(novopkt)
 
-        if ack != pkt.seq + MSS:
-            print("Erro no envio do pacote, sequencia:", pkt.seq + MSS, "ack:", ack)
+            # Atualiza o numero de sequencia do pacote
+            pkt.seq = pkt.seq + len(var)
+
+            # Atualiza a posição atual do arquivo não confirmada
+            curr_file_position += len(var)
+        
+        # Envia os pacotes
+        respondidos = sr_pkt(listaDePacotes, timeout)
+        # Ve se algum dos pacotes respondidos é o pacote de confirmação
+        ack = 0
+        for pktresp in respondidos:
+            if pktresp[1].ack > ack:
+                ack = pktresp[1].ack
+
+        if ack != pkt.seq:
             curr_file_position = curr_file_position_confirmed
         else:
-            curr_file_position = curr_file_position + MSS
-            curr_file_position_confirmed = curr_file_position - MSS
+            curr_file_position_confirmed = curr_file_position
         pkt.seq = ack
+
+        # printa o curr_file_position_confirmed e o curr_file_position e o file_size
+        print(curr_file_position_confirmed, curr_file_position, file_size)
         
     return pkt.seq, pkt.ack
     
@@ -120,7 +124,7 @@ def main():
     # Cria o pacote com os dados base
     pkt = ip_pkt / TCP(dport=dst_port, flags='PA', seq=seq, ack=ack)
     # Envia os dados
-    seq, ack = send_data(f, os.path.getsize("lotr.txt"), pkt, timeout)
+    seq, ack = send_data(f, os.path.getsize("lotr copy.txt"), pkt, timeout)
     # Enviar o pacote de finalização
     end_connection(seq, ack)
 
