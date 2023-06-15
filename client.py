f91206ab7dd9ec95ea61bc95ea9d6fbd7f4b3a08
Jubarte27 <matheusmazar@gmail.com>
2023-06-15 01:47:21 -0300

    sr não retorna pacotes que não tem anda a ver com os enviados

--- client.py
+++ client.py
@@ -1,16 +1,16 @@
-import os 
-from scapy.layers.l2 import Ether
+import os
 from scapy.layers.inet import IP, TCP
-from scapy.all import sr1, send, sniff, Raw, sr
+from scapy.all import sr1, send, sr, AsyncSniffer
 
 ip_src = '10.1.1.1'
 ip_dst = '10.1.1.2'
 dst_port = 8881
-timeout = 1
+timeout = 0.15
 # Maximum Segment Size
 MSS = 1460
 
 ip_pkt = IP(src=ip_src, dst=ip_dst)
+
 
 # Função para enviar o pacote de conexão
 def begin_connection():
@@ -38,6 +38,7 @@ def begin_connection():
 
     return seq, ack
 
+
 # Função para enviar o pacote de finalização
 def end_connection(seq, ack):
     """End connection using three-way handshake algorithm."""
@@ -55,35 +56,56 @@ def end_connection(seq, ack):
     ack_pkt = ip_pkt / TCP(dport=dst_port, flags='A', seq=response[TCP].ack, ack=response[TCP].seq + 1)
     send(ack_pkt)
 
-# Função para enviar o pacote de dados
-def send_pkt(pkt, file, curr_file_position):
-    file.seek(curr_file_position)
-    data = file.read(MSS)
-    send(pkt / data, verbose=False)
 
 # Função para enviar o pacote de dados e receber a confirmação
 def sr_pkt(pkts, timeout):
     respondidos, nao_respondidos = sr(pkts, timeout=timeout, verbose=False)
-    return respondidos
+    return respondidos, nao_respondidos
+
 
 def CriaPacote(seq, ack, data=b''):
     return ip_pkt / TCP(dport=dst_port, flags='PA', seq=seq, ack=ack) / data
 
+
+def SS(cwind, ss_thresh):
+    cwind = cwind * 2
+    return cwind, ss_thresh
+
+
+def AI(cwind, ss_thresh):
+    cwind = cwind + 1
+    return cwind, ss_thresh
+
+
+def MD(cwind, ss_thresh):
+    ss_thresh = cwind // 2
+    if ss_thresh < 2:
+        ss_thresh = 2
+    cwind = ss_thresh
+    return cwind, ss_thresh
+
+
+def isAtSlowStart(cwind, ss_thresh):
+    return cwind < ss_thresh
+
+
 def send_data(file, file_size, pkt, timeout):
-    pkts_to_send = 2
     curr_file_position = 0
     curr_file_position_confirmed = 0
     last_ack = pkt.seq
-    ss_thresh = 1000
+    ss_thresh = 16
     cwind = 1
+    RTT = 0.1
 
     print(curr_file_position_confirmed, curr_file_position, file_size)
+
+    ack_to_file_position = {}
 
     # Enquanto a posição atual do arquivo for menor que o tamanho do arquivo
     while curr_file_position_confirmed < file_size:
         listaDePacotes = []
         # For para enviar os pacotes considerando o numero de pacotes que podem ser enviados
-        for _ in range(pkts_to_send):
+        for _ in range(cwind):
             # Faz o seek no arquivo
             file.seek(curr_file_position)
             # Cria o pacote
@@ -93,41 +115,53 @@ def send_data(file, file_size, pkt, timeout):
             novopkt = CriaPacote(pkt.seq, pkt.ack, var)
             # Adiciona o pacote na lista de pacotes
             listaDePacotes.append(novopkt)
+            ack_to_file_position[novopkt.seq + len(var)] = curr_file_position + len(var)
 
             # Atualiza o numero de sequencia do pacote
             pkt.seq = pkt.seq + len(var)
 
             # Atualiza a posição atual do arquivo não confirmada
             curr_file_position += len(var)
-        
-        # Envia os pacotes
-        respondidos = sr_pkt(listaDePacotes, timeout)
-        # Ve se algum dos pacotes respondidos é o pacote de confirmação
-        ack = last_ack
-        for pktresp in respondidos:
-            if pktresp[1].ack > ack:
-                ack = pktresp[1].ack
 
-        if ack != pkt.seq: # Se o ack for diferente do seq, então houve perda de pacote
+        # Envia os pacotes
+        answered, unanswered = sr_pkt(listaDePacotes, RTT)
+        # Ve se algum dos pacotes respondidos é o pacote de confirmação
+        for response in answered:
+            if response[1].ack > last_ack:
+                last_ack = response[1].ack
+            print(response[1].ack, last_ack)
+
+
+        if len(answered) == 0:  # timeout
+            cwind, ss_thresh = MD(cwind, ss_thresh)
+            cwind = 1
             curr_file_position = curr_file_position_confirmed
-            # cwind = 1
-            # ss_thresh = max(2, pkts_to_send // 2)
-            # pkts_to_send = 1
-        else:
-            # if cwind < ss_thresh:
-            #     cwind = cwind * 2
-            # else:
-            #     cwind = cwind + 1
-            # pkts_to_send = cwind
+            pkt.seq = last_ack
+            RTT = RTT * 2
+            # print("timeout")
+        elif last_ack != pkt.seq:  # Se o ack for diferente do seq, então houve perda de pacote
+            cwind, ss_thresh = MD(cwind, ss_thresh)
+            curr_file_position_confirmed = ack_to_file_position[last_ack]
+            curr_file_position = curr_file_position_confirmed
+            pkt.seq = last_ack
+            # print("perda de pacote")
+        else:  # Se não houve perda de pacote
+            if isAtSlowStart(cwind, ss_thresh):
+                cwind, ss_thresh = SS(cwind, ss_thresh)
+            else:
+                cwind, ss_thresh = AI(cwind, ss_thresh)
             curr_file_position_confirmed = curr_file_position
-            last_ack = ack
-        pkt.seq = ack
+            pkt.seq = last_ack
+            # print("sem perda de pacote")
 
         # printa o curr_file_position_confirmed e o curr_file_position e o file_size
-        print(curr_file_position_confirmed, curr_file_position, file_size)
-        
+        # print("curr_file_position_confirmed, curr_file_position, file_size")
+        # print(curr_file_position_confirmed, curr_file_position, file_size)
+        # print("cwind, ss_thresh")
+        # print(cwind, ss_thresh)
+
     return pkt.seq, pkt.ack
-    
+
 def main():
     # Enviar o pacote de conexão
     seq, ack = begin_connection()
@@ -139,6 +173,7 @@ def main():
     seq, ack = send_data(f, os.path.getsize("lotr.txt"), pkt, timeout)
     # Enviar o pacote de finalização
     end_connection(seq, ack)
+
 
 if __name__ == '__main__':
     main()
